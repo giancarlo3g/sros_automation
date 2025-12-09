93ef993a5f492f8a25aff2a130698f3e0e58f17e
Giancarlo Garcia <giancarlo3g@gmail.com>
2025-12-09 13:18:27 -0500

    output for port_stats.py

--- gnmi/port_stats.py
+++ gnmi/port_stats.py
@@ -1,6 +1,9 @@
 from pygnmi.client import gNMIclient
 from getpass import getpass
 import json
+from google.protobuf.json_format import MessageToDict, MessageToJson
+import base64
+import pandas as pd
 
 def get_connection(router):
     # Connect to device
@@ -16,9 +19,17 @@ def get_connection(router):
         pass
     return gc
 
+def proto_to_dict(pb_msg):
+    """
+    Convert a protobuf message (the `resp` you printed) into a plain Python dict.
+    """
+    return MessageToDict(pb_msg, preserving_proto_field_name=True)
+
+
 def get_stats(gc):
     with gc:
         # Subscribe to interface counters
+        port = "1/1/c1/1"
         sub = {
             "subscription": [
                 {
@@ -29,13 +40,22 @@ def get_stats(gc):
             ]
         }
 
-        # print(
-        #     "\nTime".ljust(20),
-        #     "Port".ljust(20),
-        #     "in-octects".ljust(20),
-        # )
+        print(
+            "\nTime".ljust(25),
+            "Port".ljust(20),
+            "in-octects".ljust(20),
+        )
         for resp in gc.subscribe(sub):
-            print (resp)
+            #Convert protobuf → dict
+            notif_dict = MessageToDict(resp)
+            if notif_dict.keys() == {'update'}:
+                #Get octects
+                time_stamp = int(notif_dict["update"]["timestamp"])
+                date_time = pd.to_datetime(time_stamp, unit='ns', utc=True).tz_convert('America/New_York').strftime('%Y-%m-%d %H:%M:%S')
+                data_raw = notif_dict["update"]["update"][0]["val"]["jsonVal"]
+                data_ascii = int(base64.b64decode(data_raw).decode('utf-8').strip('"'))
+                print(date_time.ljust(24), port.ljust(20), str(data_ascii).ljust(19))
+
             
 
 def main():
